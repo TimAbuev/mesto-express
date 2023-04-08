c819d633f1f6984b286c0cb7f44b5bc8fbc5fc94
TimAbuev <tim-r10@yandex.ru>
2023-04-08 18:42:16 +0300

    feat: add default name, about, avatar to userSchema

--- models/userSchema.js
+++ models/userSchema.js
@@ -4,19 +4,19 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minLength: 2,
     maxLength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minLength: 2,
     maxLength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
