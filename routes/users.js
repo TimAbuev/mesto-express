0e65f263917aa77f2252c42d5220acb7f4cb9c5d
TimAbuev <tim-r10@yandex.ru>
2023-04-10 17:35:34 +0300

    feat: add signin && signup routes

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,9 @@ const {
 
 router.get('/', getUsers);
 router.get('/:userId', getUser);
-router.post('/', createUser);
 router.patch('/me', refreshProfile);
 router.patch('/me/avatar', refreshAvatar);
-router.post('/login', login);
+router.post('/signin', login);
+router.post('/signup', createUser);
 
 module.exports = router;
