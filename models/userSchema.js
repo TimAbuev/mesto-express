4bb14400736eba049aeb3d111f11c66887dcc615
TimAbuev <tim-r10@yandex.ru>
2023-04-19 18:29:06 +0300

    fix: add required() at userSchema

--- models/userSchema.js
+++ models/userSchema.js
@@ -52,10 +52,10 @@ module.exports = {
     avatar: Joi.string().uri(),
   }),
   profileUserSchema: Joi.object({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
   }),
   avatarUserSchema: Joi.object({
-    avatar: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+    avatar: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/).required(),
   }),
 };
