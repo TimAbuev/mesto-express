a3bf30fb5c902d5b40b2ad70ab3778129a49de26
TimAbuev <tim-r10@yandex.ru>
2023-04-20 14:12:09 +0300

    fix: change path for signin && signup

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,15 @@
 const router = require('express').Router();
 const { celebrate } = require('celebrate');
 const {
-  getUser, getUsers, createUser, refreshProfile, refreshAvatar, login, getCurrentUser,
+  getUser, getUsers, refreshProfile, refreshAvatar, getCurrentUser,
 } = require('../controllers/usersControllers'); // важно соблюдать порядок импортируемых объектов
 const auth = require('../middleware/auth');
-const { signUserSchema, profileUserSchema, avatarUserSchema } = require('../models/userSchema');
+const { profileUserSchema, avatarUserSchema } = require('../models/userSchema');
 
 router.get('/me', auth, getCurrentUser);
 router.get('/:userId', auth, getUser);
 router.get('/', auth, getUsers);
 router.patch('/me', auth, celebrate({ body: profileUserSchema }), refreshProfile);
 router.patch('/me/avatar', auth, celebrate({ body: avatarUserSchema }), refreshAvatar);
-router.post('/signin', celebrate({ body: signUserSchema }), login);
-router.post('/signup', celebrate({ body: signUserSchema }), createUser);
 
 module.exports = router;
